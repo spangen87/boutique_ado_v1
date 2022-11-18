34646176131dda16af471e6fceb73fe690592555
spangen87 <rikard.spangmyr@bestlas.se>
2022-11-18 13:33:58 +0000

    Fix the image field part 2

--- products/widgets.py
+++ products/widgets.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
-    initial_text = _('')
+    initial_text = _('Current Image')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
